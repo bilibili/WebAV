05e3a47a2a44311cc431ba55bc0497cd5cf67f6c
hughfenghen <hughfenghen@gmail.com>
2023-03-27 23:14:07 +0800

    Resource Manager

--- src/resource/__tests__/index.test.ts
+++ src/resource/__tests__/index.test.ts
@@ -1,6 +1,23 @@
-import { expect, test } from 'vitest'
+import { describe, expect, test } from 'vitest'
+import { ResourceManager, VideoResource } from '..'
 
 test('1 + 1', () => {
   expect(1 + 1).toBe(2)
   console.error(111222)
+})
+
+describe('Resource Manager', () => {
+  const resMng = new ResourceManager()
+  test('addResource', () => {
+    const res1 = new VideoResource('res1')
+    res1.zIndex = 10
+    const res2 = new VideoResource('res2')
+    res1.zIndex = 1
+
+    resMng.addResource(res1)
+    resMng.addResource(res2)
+
+    expect(resMng.getResourceList().map(it => it.name))
+      .toEqual(['res2', 'res1'])
+  })
 })
--- src/resource/index.ts
+++ src/resource/index.ts
@@ -1,4 +1,42 @@
 
-export class Resource {
+interface IPoint { x: number; y: number }
 
+class BaseResource {
+  #pos: IPoint = { x: 0, y: 0 }
+
+  get pos(): IPoint { return this.#pos }
+  set pos(pos: IPoint) { this.#pos = pos }
+
+  #visible = true
+  get visible(): boolean { return this.#visible }
+  set visible(visible: boolean) { this.#visible = visible }
+
+  #canMove = true
+  get canMove(): boolean { return this.#canMove }
+  set canMove(canMove: boolean) { this.#canMove = canMove }
+
+  #zIndex= 0
+  get zIndex(): number { return this.#zIndex}
+  set zIndex(zIndex: number) { this.#zIndex= zIndex}
+
+  constructor(public name: string) {}
+}
+
+export class VideoResource extends BaseResource {
+  constructor(name: string) {
+    super(name)
+  }
+}
+
+export class ResourceManager {
+  #resList: Array<BaseResource> = []
+
+  addResource<R extends BaseResource>(res: R): void {
+    this.#resList.push(res)
+    this.#resList = this.#resList.sort((a, b) => a.zIndex - b.zIndex)
+  }
+
+  getResourceList(): Array<BaseResource> {
+    return this.#resList
+  }
 }
