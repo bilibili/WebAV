b36f68fb153a846af242c5c5069241fbc1406add
hughfenghen <hughfenghen@gmail.com>
2023-04-03 22:00:44 +0800

    feat: ut coverage

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,10 @@
 import { defineConfig } from 'vitest/config'
 export default defineConfig({
   test: {
-    environment: 'jsdom'
+    environment: 'jsdom',
+    coverage: {
+      provider: 'istanbul' // or 'c8'
+    },
+
   },
 })
