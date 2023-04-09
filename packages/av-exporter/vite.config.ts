02d8c00f20cb93f182544e2d38341fd42ba2f9bd
hughfenghen <hughfenghen@gmail.com>
2023-04-09 21:03:12 +0800

    feat: 添加 av-exporter 模块

---
+++ packages/av-exporter/vite.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from 'vitest/config'
+export default defineConfig({
+  test: {
+    environment: 'jsdom',
+    coverage: {
+      provider: 'istanbul' // or 'c8'
+    },
+
+  },
+})
