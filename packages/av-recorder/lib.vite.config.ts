869f8bbb4ff3a3254c631920b1cd68f1e13762c0
hughfenghen <hughfenghen@gmail.com>
2023-04-15 20:35:31 +0800

    feat: deploy demo to gh-pages

--- packages/av-recorder/lib.vite.config.ts
+++ packages/av-recorder/lib.vite.config.ts
@@ -1,10 +1,10 @@
 import { resolve } from 'path'
 import { defineConfig } from 'vitest/config'
 export default defineConfig({
-  test: {
-    environment: 'jsdom',
-    coverage: {
-      provider: 'istanbul' // or 'c8'
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/av-recorder.ts'),
+      name: 'av-recorder',
     },
   }
 })
