9b35bd2ee12b136d09a94b705e123ca49b063869
hughfenghen <hughfenghen@gmail.com>
2023-04-19 23:50:30 +0800

    feat: source goup & test

---
+++ packages/av-cliper/src/__tests__/source-group.test.ts
@@ -0,0 +1,51 @@
+import { ReadableStream } from 'web-streams-polyfill/ponyfill'
+
+import { beforeAll, expect, test, vi } from 'vitest'
+import { SourceGroup } from '../source-group'
+
+;(global as any).ReadableStream = ReadableStream
+
+function createRS (): ReadableStream<any> {
+  let timerId = 0
+  return new ReadableStream({
+    start (ctrl) {
+      let i = 0
+      timerId = window.setInterval(() => {
+        if (i > 100) {
+          clearInterval(timerId)
+          ctrl.close()
+          return
+        }
+        ctrl.enqueue({
+          timestamp: i,
+          data: Math.random()
+        })
+        i += 10
+      }, 10)
+    },
+    cancel () {
+      console.log('-------end---------')
+      clearInterval(timerId)
+    }
+  })
+}
+
+beforeAll(() => {
+  vi.useFakeTimers()
+})
+
+test('SourceGroup', async () => {
+  const sg = new SourceGroup()
+  // createRS()
+  sg.add({ start: 0, end: 1e2 }, createRS())
+  sg.start()
+  const reader = sg.outputStream.getReader()
+  const count = vi.fn()
+  while (true) {
+    vi.advanceTimersToNextTimer()
+    const { done } = await reader.read()
+    if (done) break
+    count()
+  }
+  expect(count).toBeCalledTimes(11)
+})
---
+++ packages/av-cliper/src/source-group.ts
@@ -0,0 +1,94 @@
+
+interface IDuration {
+  start: number
+  end: number
+}
+
+enum EState {
+  Pending = 'pending',
+  Reading = 'reading',
+  Done = 'done'
+}
+
+interface IItem {
+  dur: IDuration
+  source: ReadableStream
+  reader: ReadableStreamDefaultReader<{ timestamp: number }>
+  state: EState
+}
+
+export class SourceGroup {
+  #items: IItem[] = []
+
+  #ts = 0
+
+  #state = 'pending'
+
+  #ctrl: ReadableStreamDefaultController | null = null
+
+  outputStream: ReadableStream<any>
+
+  constructor () {
+    this.outputStream = new ReadableStream({
+      start: (ctrl) => {
+        this.#ctrl = ctrl
+      }
+    })
+  }
+
+  add (dur: IDuration, source: ReadableStream): void {
+    this.#items.push({
+      dur,
+      source,
+      reader: source.getReader(),
+      state: EState.Pending
+    })
+  }
+
+  start (): void {
+    if (this.#state !== 'running' && this.#ctrl != null) {
+      this.#state = 'running'
+      this.#run(this.#ctrl).catch(this.#ctrl.error)
+    }
+  }
+
+  async #run (ctrl: ReadableStreamDefaultController): Promise<void> {
+    const it = this.#findNext(this.#ts)
+    if (it == null) {
+      this.#ctrl?.close()
+      return
+    }
+
+    const { done, value } = await it.reader.read()
+    if (done) {
+      it.state = EState.Done
+      this.#run(ctrl).catch(ctrl.error)
+      return
+    }
+
+    // todo：发送一个数组
+    ctrl.enqueue(value)
+    this.#ts = value.timestamp
+    this.#run(ctrl).catch(ctrl.error)
+  }
+
+  // todo: 应该返回 IItem[]
+  #findNext (ts: number): IItem | null {
+    const items = this.#items.filter(it => it.state !== EState.Done)
+    const next = items.find(it => it.dur.start <= ts)
+
+    if (next != null) return next
+
+    let min = Infinity
+    let idx: number | null = null
+    items.forEach((it, i) => {
+      const v = it.dur.start - ts
+      if (v < min) {
+        min = v
+        idx = i
+      }
+    })
+
+    return idx != null ? this.#items[idx] : null
+  }
+}
