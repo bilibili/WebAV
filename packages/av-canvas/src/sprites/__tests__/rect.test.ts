ff163262ba8ec64ac06c6f091db6d155993f48a8
hughfenghen <hughfenghen@gmail.com>
2023-05-07 21:28:27 +0800

    docs: update plan

--- packages/av-canvas/src/sprites/__tests__/rect.test.ts
+++ packages/av-canvas/src/sprites/__tests__/rect.test.ts
@@ -1,5 +1,5 @@
 import { describe, expect, test } from 'vitest'
-import { Rect, TAnimationKeyFrame, linearTimeFn } from '../rect'
+import { Rect } from '../rect'
 
 describe('Rect', () => {
   test('center', () => {
