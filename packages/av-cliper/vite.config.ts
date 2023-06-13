0cf7e6dd397fceccaf81875016adec949c8bf486
hughfenghen <hughfenghen@gmail.com>
2023-06-13 18:09:52 +0800

    fix: misspell

--- packages/av-cliper/vite.config.ts
+++ packages/av-cliper/vite.config.ts
@@ -13,7 +13,7 @@ export default defineConfig({
     lib: {
       entry: resolve(__dirname, 'src/index.ts'),
       formats: ['es'],
-      fileName: 'idnex'
+      fileName: 'index'
     }
   }
 })
