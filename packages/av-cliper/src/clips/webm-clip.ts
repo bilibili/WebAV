52988acf8f883aa3f441cff0d3f62e578bead4f7
hughfenghen <hughfenghen@gmail.com>
2023-06-29 19:12:05 +0800

    feat: decode vp9

--- packages/av-cliper/src/clips/webm-clip.ts
+++ packages/av-cliper/src/clips/webm-clip.ts
@@ -1,8 +1,9 @@
-import JsWebm from 'jswebm'
 import OGVDemuxerWebMW from 'ogv/dist/ogv-demuxer-webm-wasm'
+import OGVDecoderVideoVP9W from 'ogv/dist/ogv-decoder-video-vp9-wasm'
 import { autoReadStream } from '../av-utils'
 
 import { IClip } from './iclip'
+import { Log } from '../log'
 
 export class WebMClip implements IClip {
   ready: IClip['ready']
@@ -15,30 +16,73 @@ export class WebMClip implements IClip {
       end?: number
     } = {}
   ) {
-    const webm = new JsWebm()
-
     this.ready = new Promise(async resolve => {
       const demuxer = await OGVDemuxerWebMW()
-      demuxer.onseek = offset => {
-        console.log('--- onseek', offset)
-      }
+      const decoder = await OGVDecoderVideoVP9W()
       demuxer.init(() => {})
+      decoder.init(() => {})
 
       demuxer.ready.then(() => {
-        console.log(77777777777, demuxer.audioReady)
-        console.log(4444, demuxer)
+        console.log(4444, demuxer, decoder)
       })
+
+      function process (rs: boolean) {
+        if (rs) demuxer.process(process)
+        else decode()
+      }
+
+      function decode () {
+        // const decoder = new VideoDecoder({
+        //   error: Log.error,
+        //   output: vf => {
+        //     console.log(1111111, vf, decoder.decodeQueueSize)
+        //     const buf = new ArrayBuffer(vf.allocationSize())
+        //     vf.copyTo(buf).then(d => console.log(6666, buf))
+        //     ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height)
+        //     ctx.drawImage(vf, 0, 0)
+        //     // vf.close()
+        //   }
+        // })
+        // decoder.configure({
+        //   codec: 'vp09.00.20.08',
+        //   codedHeight: demuxer.videoFormat.height,
+        //   codedWidth: demuxer.videoFormat.width
+        // })
+        console.log(5555, demuxer.videoPackets)
+        demuxer.videoPackets.forEach((p, idx) => {
+          if (idx === 0) {
+            decoder.processHeader(p, ok => {
+              console.log(6666, ok)
+            })
+          } else {
+            decoder.processFrame(p, ok => {
+              // hack
+              let fb = decoder.frameBuffer
+              console.log(6666, fb, ok)
+            })
+          }
+        })
+        // for (const p of demuxer.videoPackets) {
+        // decoder.decode(
+        //   new EncodedVideoChunk({
+        //     type: (p.isKeyframe ? 'key' : 'delta') as EncodedVideoChunkType,
+        //     timestamp: 1e6 * p.timestamp,
+        //     data: p.data
+        //   })
+        // )
+        // }
+      }
 
       autoReadStream(rs, {
         onDone () {
-          console.log(9999)
-          setInterval(() => {
-            console.log(444)
-            demuxer.process(() => {})
-            // demuxer.dequeueVideoPacket(packet => {
-            //   console.log('-- webm packet', packet)
-            // })
-          }, 100)
+          let i = 0
+          demuxer.process(process)
+          // setInterval(() => {
+          //   demuxer.dequeueVideoPacket(packet => {
+          //     if (packet != null) i++
+          //     console.log('-- webm packet', packet, i)
+          //   })
+          // }, 10)
 
           // demuxer.flush(() => {})
           // demuxer.dequeueVideoPacket(packet => {
@@ -53,9 +97,6 @@ export class WebMClip implements IClip {
         },
         async onChunk (buf) {
           demuxer.receiveInput(buf.buffer, () => {})
-
-          // webm.queueData(buf.buffer)
-          // webm.demux()
         }
       })
     })
