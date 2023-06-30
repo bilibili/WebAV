d9ac06d72840b65c8b64457969dfc0e0adba9e8d
hughfenghen <hughfenghen@gmail.com>
2023-06-30 18:17:14 +0800

    feat: ouput umd.cjs

--- packages/av-recorder/vite.config.ts
+++ packages/av-recorder/vite.config.ts
@@ -1,9 +1,18 @@
 import { defineConfig } from 'vitest/config'
+import { resolve } from 'path'
+
 export default defineConfig({
   test: {
     environment: 'jsdom',
     coverage: {
       provider: 'istanbul' // or 'c8'
+    }
+  },
+  build: {
+    sourcemap: true,
+    lib: {
+      entry: resolve(__dirname, 'src/av-recorder.ts'),
+      name: 'av-recoder'
     }
   }
 })
