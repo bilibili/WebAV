731ed9cd74e2a9be4c5dc68e57cc7e1b6c8c99fa
hughfenghen <hughfenghen@gmail.com>
2023-07-07 11:09:04 +0800

    chore: deploy chromakey demo

--- packages/av-cliper/demo.vite.config.ts
+++ packages/av-cliper/demo.vite.config.ts
@@ -7,6 +7,7 @@ export default defineConfig({
       input: {
         'concat-media': resolve(__dirname, 'demo/concat-media.html'),
         'decode-media': resolve(__dirname, 'demo/decode-media.html'),
+        chromakey: resolve(__dirname, 'demo/chromakey.html'),
         'fast-concat-mp4': resolve(__dirname, 'demo/fast-concat-mp4.html')
       }
     },
