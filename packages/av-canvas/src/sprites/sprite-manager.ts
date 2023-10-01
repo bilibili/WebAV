a46bf8a3c44f3cda917412cc016dc3160f426752
hughfenghen <hughfenghen@gmail.com>
2023-10-01 17:00:43 +0800

    feat: Sort in ascending order by sprite.zIndex

--- packages/av-canvas/src/sprites/sprite-manager.ts
+++ packages/av-canvas/src/sprites/sprite-manager.ts
@@ -22,14 +22,14 @@ export class SpriteManager {
 
   on = this.#evtTool.on
 
-  get activeSprite (): BaseSprite | null { return this.#activeSprite }
-  set activeSprite (s: BaseSprite | null) {
+  get activeSprite(): BaseSprite | null { return this.#activeSprite }
+  set activeSprite(s: BaseSprite | null) {
     if (s === this.#activeSprite) return
     this.#activeSprite = s
     this.#evtTool.emit(ESpriteManagerEvt.ActiveSpriteChange, s)
   }
 
-  constructor () {
+  constructor() {
     this.#bgAudio()
   }
 
@@ -42,16 +42,23 @@ export class SpriteManager {
     this.#evtTool.emit(ESpriteManagerEvt.AddSprite, s)
   }
 
-  removeSprite (spr: BaseSprite): void {
+  /**
+   * Sort in ascending order by sprite.zIndex
+   */
+  sortSprite() {
+    this.#sprites = this.#sprites.sort((a, b) => a.zIndex - b.zIndex)
+  }
+
+  removeSprite(spr: BaseSprite): void {
     this.#sprites = this.#sprites.filter(s => s !== spr)
     spr.destroy()
   }
 
-  getSprites (): BaseSprite[] {
+  getSprites(): BaseSprite[] {
     return [...this.#sprites]
   }
 
-  destroy (): void {
+  destroy(): void {
     this.#evtTool.destroy()
     this.#sprites.forEach(s => s.destroy())
     this.#sprites = []
@@ -60,7 +67,7 @@ export class SpriteManager {
   }
 
   // 添加背景音，如果没有音频，录制的webm不正常
-  #bgAudio (): void {
+  #bgAudio(): void {
     const oscillator = this.audioCtx.createOscillator()
     const wave = this.audioCtx.createPeriodicWave(
       new Float32Array([0, 0]),
