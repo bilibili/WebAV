1be56ea4f891151e29a19030c91c212f5c11defe
hughfenghen <hughfenghen@gmail.com>
2023-12-01 21:23:59 +0800

    docs: improve demo

--- doc-site/docs/demo/decode-image.tsx
+++ doc-site/docs/demo/decode-image.tsx
@@ -9,8 +9,11 @@ const imgs = {
   gif: '/img/animated.gif',
 };
 
-let stopImg = () => {};
-async function decode(ctx, imgType: keyof typeof imgs) {
+let stopRender = () => {};
+async function start(
+  ctx: CanvasRenderingContext2D,
+  imgType: keyof typeof imgs,
+) {
   const frames = await decodeImg(
     (
       await fetch(imgs[imgType])
@@ -18,7 +21,7 @@ async function decode(ctx, imgType: keyof typeof imgs) {
     `image/${imgType}`,
   );
 
-  stopImg();
+  stopRender();
   let i = 0;
   function render(vf: VideoFrame) {
     if (vf == null) return;
@@ -30,49 +33,55 @@ async function decode(ctx, imgType: keyof typeof imgs) {
       vf.close();
     }, (vf.duration ?? 0) / 1000);
 
-    stopImg = () => {
+    stopRender = () => {
       clearTimeout(timer);
     };
   }
   render(frames[0]);
 }
 
-export default () => {
-  const [imgType, setImgType] = useState('avif');
+export default createUI(start);
 
-  const [ctx, setCtx] = useState<null | undefined | CanvasRenderingContext2D>(
-    null,
-  );
+// ---------- 以下是 UI 代码 ---------------
 
-  return (
-    <div>
-      <Button
-        onClick={() => {
-          decode(ctx, imgType as keyof typeof imgs);
-        }}
-      >
-        启动！
-      </Button>
-      <br />
-      <Radio.Group
-        onChange={(e) => {
-          setImgType(e.target.value);
-        }}
-        value={imgType}
-      >
-        <Radio value="avif">avif</Radio>
-        <Radio value="webp">webp</Radio>
-        <Radio value="png">png</Radio>
-        <Radio value="gif">gif</Radio>
-      </Radio.Group>
-      <br />
-      <canvas
-        width={600}
-        height={333}
-        ref={(c) => {
-          setCtx(c?.getContext('2d'));
-        }}
-      />
-    </div>
-  );
-};
+function createUI(start: Function) {
+  return () => {
+    const [imgType, setImgType] = useState('avif');
+
+    const [ctx, setCtx] = useState<null | undefined | CanvasRenderingContext2D>(
+      null,
+    );
+
+    return (
+      <div>
+        <Button
+          onClick={() => {
+            start(ctx, imgType as keyof typeof imgs);
+          }}
+        >
+          启动！
+        </Button>
+        <br />
+        <Radio.Group
+          onChange={(e) => {
+            setImgType(e.target.value);
+          }}
+          value={imgType}
+        >
+          <Radio value="avif">avif</Radio>
+          <Radio value="webp">webp</Radio>
+          <Radio value="png">png</Radio>
+          <Radio value="gif">gif</Radio>
+        </Radio.Group>
+        <br />
+        <canvas
+          width={600}
+          height={333}
+          ref={(c) => {
+            setCtx(c?.getContext('2d'));
+          }}
+        />
+      </div>
+    );
+  };
+}
