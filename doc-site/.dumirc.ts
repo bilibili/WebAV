220352c49102b3220d91538af0af47609d6749b1
hughfenghen <hughfenghen@gmail.com>
2023-12-03 20:16:30 +0800

    chore: doc site socialLinks

--- doc-site/.dumirc.ts
+++ doc-site/.dumirc.ts
@@ -4,6 +4,9 @@ export default defineConfig({
   themeConfig: {
     name: 'WebAV',
     logo: false,
+    socialLinks: {
+      github: 'https://github.com/hughfenghen/WebAV'
+    },
     footer: 'Powered by <a href="https://github.com/hughfenghen/WebAV">WebAV</a>'
   },
   ...(process.env.NODE_ENV === 'development' ? {
