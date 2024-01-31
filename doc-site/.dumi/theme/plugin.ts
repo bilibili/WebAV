6cd8b655ba21c329ee8fb836b273e311578575e1
hughfenghen <hughfenghen@gmail.com>
2024-01-31 17:01:41 +0800

    feat: doc-site add clarity

--- doc-site/.dumi/theme/plugin.ts
+++ doc-site/.dumi/theme/plugin.ts
@@ -16,4 +16,12 @@ export default function (api) {
       document.body.appendChild(s)
     }, 100);
   `)
+
+  api.addHTMLHeadScripts(() => `
+    (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "kudfzh7lis");
+  `)
 }
