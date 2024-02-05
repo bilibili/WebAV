9354613fd512b9541e7a6d7d1bad8651ea5c3753
hughfenghen <hughfenghen@gmail.com>
2024-02-05 11:50:28 +0800

    fix: hide codesandbox icon, because not working

---
+++ doc-site/.dumi/theme/builtins/Previewer.tsx
@@ -0,0 +1,5 @@
+// @ts-nocheck
+import Previewer from 'dumi/theme-default/builtins/Previewer';
+export default (props) => {
+  return <Previewer {...props} disabledActions={['CSB', 'STACKBLITZ']} />;
+};
--- doc-site/.dumi/theme/plugin.ts
+++ doc-site/.dumi/theme/plugin.ts
@@ -1,7 +1,8 @@
-
+// @ts-nocheck
 export default function (api) {
   // github-button
-  api.addHTMLHeadScripts(() => `
+  api.addHTMLHeadScripts(
+    () => `
     var timer = setInterval(() => {
       if (performance.now() > 10000) {
         clearInterval(timer)
@@ -15,13 +16,16 @@ export default function (api) {
       s.src = 'https://buttons.github.io/buttons.js'
       document.body.appendChild(s)
     }, 100);
-  `)
+  `,
+  );
 
-  api.addHTMLHeadScripts(() => `
+  api.addHTMLHeadScripts(
+    () => `
     (function(c,l,a,r,i,t,y){
         c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
         t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
         y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
     })(window, document, "clarity", "script", "kudfzh7lis");
-  `)
+  `,
+  );
 }
