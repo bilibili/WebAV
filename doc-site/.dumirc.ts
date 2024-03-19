0091f94eb4b08f1cd176932ed3d42d12436f2dcc
hughfenghen <hughfenghen@gmail.com>
2024-03-19 16:30:55 +0800

    docs: alias packages

--- doc-site/.dumirc.ts
+++ doc-site/.dumirc.ts
@@ -1,8 +1,24 @@
 import { defineConfig } from 'dumi';
+import path from 'path';
 
 export default defineConfig({
   plugins: ['@umijs/plugins/dist/tailwindcss'],
   tailwindcss: {}, //在umi中，表示启用该插件
+  alias: {
+    '@webav/av-cliper': path.resolve(
+      __dirname,
+      '../packages/av-cliper/src/index.ts',
+    ),
+    // av-recorder 使用 vite 的 import worker，umi 不兼容
+    // '@webav/av-recorder': path.resolve(
+    //   __dirname,
+    //   '../packages/av-recorder/src/av-recorder.ts',
+    // ),
+    '@webav/av-canvas': path.resolve(
+      __dirname,
+      '../packages/av-canvas/src/index.ts',
+    ),
+  },
 
   analytics: { ga_v2: 'G-MC335K4KV6' },
   themeConfig: {
