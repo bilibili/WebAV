d7590557cadbe443d7803c293d7477ca4a77c654
LHJ <408980401@qq.com>
2024-03-22 15:53:17 +0800

    testing git hooks

---
+++ scripts/verify-commit.js
@@ -0,0 +1,27 @@
+import pico from 'picocolors';
+import { readFileSync } from 'node:fs';
+import path from 'node:path';
+
+const msgPath = path.resolve('.git/COMMIT_EDITMSG');
+const msg = readFileSync(msgPath, 'utf-8').trim();
+
+const commitRE =
+  /^(revert: )?(feat|fix|docs|dx|style|refactor|perf|test|workflow|build|ci|chore|types|wip|release)(\(.+\))?: .{1,50}/;
+
+if (!commitRE.test(msg)) {
+  console.log();
+  console.error(
+    `  ${pico.white(pico.bgRed(' ERROR '))} ${pico.red(
+      `invalid commit message format.`,
+    )}\n\n` +
+      pico.red(
+        `  Proper commit message format is required for automated changelog generation. Examples:\n\n`,
+      ) +
+      `    ${pico.green(`feat(av-canvas): add 'comments' option`)}\n` +
+      `    ${pico.green(
+        `fix(event-tool): clear listeners on listener interface (close #00)`,
+      )}\n\n` +
+      pico.red(`  See .github/commit-convention.md for more details.\n`),
+  );
+  process.exit(1);
+}
