22f44a3b1062e4f44ebc999dc73fe29efb0ead0e
hughfenghen <hughfenghen@gmail.com>
2024-03-24 15:13:15 +0800

    docs: fix footer style

--- doc-site/.dumirc.ts
+++ doc-site/.dumirc.ts
@@ -28,7 +28,7 @@ export default defineConfig({
       github: 'https://github.com/hughfenghen/WebAV',
     },
     footer:
-      '碰到问题请去 <a href="https://github.com/hughfenghen/WebAV/issues/">WebAV Issues</a> 中反馈 <br/> <a class="github-button" href="https://github.com/hughfenghen/WebAV" data-color-scheme="no-preference: light; light: light; dark: dark;" data-icon="octicon-star" data-size="large" data-show-count="true" aria-label="Star hughfenghen/WebAV on GitHub">Star</a>',
+      '<div>碰到问题请去 <a href="https://github.com/hughfenghen/WebAV/issues/">WebAV Issues</a> 中反馈</div><div class="flex" style="justify-content: center;"><a class="github-button" href="https://github.com/hughfenghen/WebAV" data-color-scheme="no-preference: light; light: light; dark: dark;" data-icon="octicon-star" data-size="large" data-show-count="true" aria-label="Star hughfenghen/WebAV on GitHub">Star</a></div>',
   },
   ...(process.env.NODE_ENV === 'development'
     ? {
