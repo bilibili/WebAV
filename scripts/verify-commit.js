0a132cd3bfe1d5cad3e2dccea3acea782de304a8
liwakin <liwakin@qq.com>
2024-03-24 17:25:33 +0800

    chore: 去除commit字数限制

--- scripts/verify-commit.js
+++ scripts/verify-commit.js
@@ -6,7 +6,7 @@ const msgPath = resolve('.git/COMMIT_EDITMSG');
 const msg = readFileSync(msgPath, 'utf-8').trim();
 
 const commitRE =
-  /^(revert: )?(feat|fix|docs|dx|style|refactor|perf|test|workflow|build|ci|chore|types|wip|release)(\(.+\))?: .{1,50}/;
+  /^(revert: )?(feat|fix|docs|dx|style|refactor|perf|test|workflow|build|ci|chore|types|wip|release)(\(.+\))?: .+/;
 
 if (!commitRE.test(msg)) {
   console.log();
