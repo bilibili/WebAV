d54314a596e3409f3adc534fd6f30b64fc6e30b9
hughfenghen <hughfenghen@gmail.com>
2024-05-04 20:18:29 +0800

    refactor: deprecated old sprite

--- packages/av-canvas/src/index.ts
+++ packages/av-canvas/src/index.ts
@@ -1,6 +1,1 @@
 export { AVCanvas } from './av-canvas';
-
-export { AudioSprite } from './sprites/audio-sprite';
-export { VideoSprite } from './sprites/video-sprite';
-export { TextSprite } from './sprites/text-sprite';
-export { ImgSprite } from './sprites/img-sprite';
