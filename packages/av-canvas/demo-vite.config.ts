c2a021d50aded4089c5b907200bec93ceb3eae03
hughfenghen <hughfenghen@gmail.com>
2024-05-05 15:31:55 +0800

    refactor: AVCanvas demo

---
+++ packages/av-canvas/demo-vite.config.ts
@@ -0,0 +1,10 @@
+import { resolve } from 'path';
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+  server: {
+    host: '0.0.0.0',
+    port: 6066,
+  },
+  publicDir: resolve(__dirname, '../../doc-site/public'),
+});
--- packages/av-canvas/demo/record-avcanvas.ts
+++ packages/av-canvas/demo/record-avcanvas.ts
@@ -1,24 +1,17 @@
-import { Combinator } from '@webav/av-cliper';
+import { Sprite, MP4Clip } from '@webav/av-cliper';
 import {
   AVCanvas,
-  AudioSprite,
-  TextSprite,
-  ImgSprite,
-  VideoSprite,
+  //   AudioSprite,
+  //   TextSprite,
+  //   ImgSprite,
+  //   VideoSprite,
 } from '../src/index';
 import { AVRecorder } from '@webav/av-recorder';
-(async () => {
-  if (!(await Combinator.isSupported())) {
-    alert('Your browser does not support WebCodecs');
-  }
-})();
 
 const avCvs = new AVCanvas(document.querySelector('#app') as HTMLElement, {
   bgColor: '#333',
-  resolution: {
-    width: 1920,
-    height: 1080,
-  },
+  width: 1920,
+  height: 1080,
 });
 
 console.log({ avCvs });
@@ -28,90 +21,92 @@ console.log({ avCvs });
 })().catch(console.error);
 document.querySelector('#userMedia')?.addEventListener('click', () => {
   (async () => {
-    const mediaStream = await navigator.mediaDevices.getUserMedia({
-      video: true,
-      audio: true,
-    });
-    const vs = new VideoSprite('userMedia', mediaStream, {
-      audioCtx: avCvs.spriteManager.audioCtx,
-    });
-    await avCvs.spriteManager.addSprite(vs);
+    // const mediaStream = await navigator.mediaDevices.getUserMedia({
+    //   video: true,
+    //   audio: true,
+    // });
+    // const vs = new VideoSprite('userMedia', mediaStream, {
+    //   audioCtx: avCvs.spriteManager.audioCtx,
+    // });
+    // await avCvs.spriteManager.addSprite(vs);
+    const clip = new MP4Clip((await fetch('./video/bunny.mp4')).body!);
+    await avCvs.spriteManager.addSprite(new Sprite(clip));
   })().catch(console.error);
 });
 
 document.querySelector('#display')?.addEventListener('click', () => {
-  (async () => {
-    const mediaStream = await navigator.mediaDevices.getDisplayMedia({
-      video: true,
-      audio: true,
-    });
-    const vs = new VideoSprite('display', mediaStream, {
-      audioCtx: avCvs.spriteManager.audioCtx,
-    });
-    await avCvs.spriteManager.addSprite(vs);
-  })().catch(console.error);
+  // (async () => {
+  //   const mediaStream = await navigator.mediaDevices.getDisplayMedia({
+  //     video: true,
+  //     audio: true,
+  //   });
+  //   const vs = new VideoSprite('display', mediaStream, {
+  //     audioCtx: avCvs.spriteManager.audioCtx,
+  //   });
+  //   await avCvs.spriteManager.addSprite(vs);
+  // })().catch(console.error);
 });
 
 document.querySelector('#localImg')?.addEventListener('click', () => {
-  (async () => {
-    const [imgFH] = await (window as any).showOpenFilePicker({
-      types: [
-        {
-          description: 'Images',
-          accept: {
-            'image/*': ['.png', '.gif', '.jpeg', '.jpg'],
-          },
-        },
-      ],
-    });
-    const is = new ImgSprite('img', await imgFH.getFile());
-    await avCvs.spriteManager.addSprite(is);
-  })().catch(console.error);
+  // (async () => {
+  //   const [imgFH] = await (window as any).showOpenFilePicker({
+  //     types: [
+  //       {
+  //         description: 'Images',
+  //         accept: {
+  //           'image/*': ['.png', '.gif', '.jpeg', '.jpg'],
+  //         },
+  //       },
+  //     ],
+  //   });
+  //   const is = new ImgSprite('img', await imgFH.getFile());
+  //   await avCvs.spriteManager.addSprite(is);
+  // })().catch(console.error);
 });
 
 document.querySelector('#localVideo')?.addEventListener('click', () => {
-  (async () => {
-    const [imgFH] = await (window as any).showOpenFilePicker({
-      types: [
-        {
-          description: 'Video',
-          accept: {
-            'video/*': ['.webm', '.mp4'],
-          },
-        },
-      ],
-    });
-    const vs = new VideoSprite('vs', await imgFH.getFile(), {
-      audioCtx: avCvs.spriteManager.audioCtx,
-    });
-    await avCvs.spriteManager.addSprite(vs);
-  })().catch(console.error);
+  // (async () => {
+  //   const [imgFH] = await (window as any).showOpenFilePicker({
+  //     types: [
+  //       {
+  //         description: 'Video',
+  //         accept: {
+  //           'video/*': ['.webm', '.mp4'],
+  //         },
+  //       },
+  //     ],
+  //   });
+  //   const vs = new VideoSprite('vs', await imgFH.getFile(), {
+  //     audioCtx: avCvs.spriteManager.audioCtx,
+  //   });
+  //   await avCvs.spriteManager.addSprite(vs);
+  // })().catch(console.error);
 });
 
 document.querySelector('#localAudio')?.addEventListener('click', () => {
-  (async () => {
-    const [imgFH] = await (window as any).showOpenFilePicker({
-      types: [
-        {
-          description: 'Audio',
-          accept: {
-            'audio/*': ['.mp3', '.wav', '.ogg'],
-          },
-        },
-      ],
-    });
-    const as = new AudioSprite('vs', await imgFH.getFile(), {
-      audioCtx: avCvs.spriteManager.audioCtx,
-    });
-    await avCvs.spriteManager.addSprite(as);
-  })().catch(console.error);
+  // (async () => {
+  //   const [imgFH] = await (window as any).showOpenFilePicker({
+  //     types: [
+  //       {
+  //         description: 'Audio',
+  //         accept: {
+  //           'audio/*': ['.mp3', '.wav', '.ogg'],
+  //         },
+  //       },
+  //     ],
+  //   });
+  //   const as = new AudioSprite('vs', await imgFH.getFile(), {
+  //     audioCtx: avCvs.spriteManager.audioCtx,
+  //   });
+  //   await avCvs.spriteManager.addSprite(as);
+  // })().catch(console.error);
 });
 
 document.querySelector('#fontExamp')?.addEventListener('click', () => {
-  (async () => {
-    const textSpr = new TextSprite('text', '示例文字');
-    await avCvs.spriteManager.addSprite(textSpr);
-  })().catch(console.error);
+  // (async () => {
+  //   const textSpr = new TextSprite('text', '示例文字');
+  //   await avCvs.spriteManager.addSprite(textSpr);
+  // })().catch(console.error);
 });
 
 let recorder: AVRecorder | null = null;
