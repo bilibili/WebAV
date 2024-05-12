3d7f74b0a79ab425d2c3bc1b65f105634827729a
hughfenghen <hughfenghen@gmail.com>
2024-05-12 20:50:28 +0800

    feat: video editor demo

--- packages/av-canvas/demo/video-editor.tsx
+++ packages/av-canvas/demo/video-editor.tsx
@@ -16,23 +16,44 @@ function App() {
   const [avCvs, setAVCvs] = useState<AVCanvas | null>(null);
   const [cvsWrapEl, setCvsWrapEl] = useState<HTMLDivElement | null>(null);
   const [clips, setClips] = useState<TimelineRow[]>([
-    { id: '0', actions: [] },
-    { id: '1', actions: [] },
-    { id: '2', actions: [] },
-    { id: '3', actions: [] },
+    { id: '1-video', actions: [] },
+    { id: '2-audio', actions: [] },
+    { id: '3-img', actions: [] },
+    { id: '4-text', actions: [] },
   ]);
 
   useEffect(() => {
     if (cvsWrapEl == null) return;
     avCvs?.destroy();
-    setAVCvs(
-      new AVCanvas(cvsWrapEl, {
-        bgColor: '#000',
-        width: 1280,
-        height: 720,
-      })
-    );
+    const cvs = new AVCanvas(cvsWrapEl, {
+      bgColor: '#000',
+      width: 1280,
+      height: 720,
+    });
+    setAVCvs(cvs);
+
+    return () => {
+      cvs?.destroy();
+    };
   }, [cvsWrapEl]);
+
+  function addItem2Track(trackId: string) {
+    const track = clips.find(({ id }) => id === trackId);
+    if (track == null) return;
+    const maxTime = Math.max(...track.actions.map((a) => a.end), 0);
+    track.actions.push({
+      id: Math.random().toString(),
+      start: maxTime,
+      end: maxTime + 2,
+      effectId: '',
+    });
+    setClips(
+      clips
+        .filter((it) => it !== track)
+        .concat({ ...track })
+        .sort((a, b) => a.id.charCodeAt(0) - b.id.charCodeAt(0)),
+    );
+  }
 
   return (
     <div className="canvas-wrap">
@@ -40,28 +61,35 @@ function App() {
       <button
         className="mx-[10px]"
         onClick={() => {
-          const track = clips.find(({ id }) => id === '0');
-          if (track == null) return;
-          const maxTime = Math.max(...track.actions.map((a) => a.end));
-          track.actions.push({
-            id: Math.random().toString(),
-            start: maxTime,
-            end: maxTime + 2,
-            effectId: '',
-          });
-          setClips(
-            clips
-              .filter((it) => it !== track)
-              .concat({ ...track })
-              .sort((a, b) => Number(a.id) - Number(b.id))
-          );
+          addItem2Track('1-video');
         }}
       >
         + 视频
       </button>
-      <button className="mx-[10px]">+ 音频</button>
-      <button className="mx-[10px]">+ 图片</button>
-      <button className="mx-[10px]">+ 文字</button>
+      <button
+        className="mx-[10px]"
+        onClick={() => {
+          addItem2Track('2-audio');
+        }}
+      >
+        + 音频
+      </button>
+      <button
+        className="mx-[10px]"
+        onClick={() => {
+          addItem2Track('3-img');
+        }}
+      >
+        + 图片
+      </button>
+      <button
+        className="mx-[10px]"
+        onClick={() => {
+          addItem2Track('4-text');
+        }}
+      >
+        + 文字
+      </button>
       <TimelineEditor clips={clips}></TimelineEditor>
     </div>
   );
