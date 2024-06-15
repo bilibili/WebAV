7b7ddd600b4781293ca4a18d223a8c0dcc779274
izkizk8 <izkizk8@qq.com>
2024-06-15 15:20:49 +0800

    fix(demo): disable csb

--- doc-site/.dumi/theme/builtins/Previewer.tsx
+++ doc-site/.dumi/theme/builtins/Previewer.tsx
@@ -1,5 +1,5 @@
 // @ts-nocheck
 import Previewer from 'dumi/theme-default/builtins/Previewer';
 export default (props) => {
-  return <Previewer {...props} />;
+  return <Previewer {...props} disabledActions={['CSB']} />;
 };
