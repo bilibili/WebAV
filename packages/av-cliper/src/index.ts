4c62dfdc6824ee09445cdd8a6aeaaa87eb74a58f
hughfenghen <hughfenghen@gmail.com>
2024-10-12 14:36:17 +0800

    refactor: remove unused exports and reorganize imports [BREAKING]

--- packages/av-cliper/src/index.ts
+++ packages/av-cliper/src/index.ts
@@ -1,16 +1,5 @@
-export * from './dom-utils';
-export {
-  concatFloat32Array,
-  concatPCMFragments,
-  extractPCM4AudioData,
-  extractPCM4AudioBuffer,
-  adjustAudioDataVolume,
-  decodeImg,
-  mixinPCM,
-  audioResample,
-  ringSliceFloat32Array,
-  autoReadStream,
-} from './av-utils';
+export { renderTxt2ImgBitmap } from './dom-utils';
+export { autoReadStream } from './av-utils';
 export {
   recodemux,
   file2stream,
@@ -18,11 +7,18 @@ export {
   fixFMP4Duration,
   mixinMP4AndAudio,
 } from './mp4-utils';
-export { createHLSLoader } from './data-loader/hls-loader';
 export { createChromakey } from './chromakey';
 export { workerTimer } from './worker-timer';
 
-export * from './clips';
+export { MP4Clip } from './clips';
+export type {
+  IClip,
+  ImgClip,
+  AudioClip,
+  DEFAULT_AUDIO_CONF,
+  MediaStreamClip,
+  EmbedSubtitlesClip,
+} from './clips';
 export { OffscreenSprite } from './sprite/offscreen-sprite';
 export { VisibleSprite } from './sprite/visible-sprite';
 export { Rect } from './sprite/rect';
